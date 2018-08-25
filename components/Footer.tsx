e227a8ceb893703d547925e76e3f257837a243fc
ryoma kawahara <akahirefriend2008@yahoo.com>
2018-08-25 14:12:57 +0900

    Add icons to Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default class Footer extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <TouchableOpacity>
+          <FontAwesome5 name="home" size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <FontAwesome5 name="search" size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <FontAwesome5 name="bell" size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <FontAwesome5 name="envelope" size={30} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -14,10 +27,12 @@ export default class Footer extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     width: '100%',
     borderTopWidth: 0.5,
+    paddingHorizontal: 40,
   },
 });
